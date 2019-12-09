e17e1b35503bd3f4b4f5dbc37cfb6aa001ba5184
AdityaGawde97 <gawde.aditya97@gmail.com>
2019-12-09 19:35:54 +0530

    login and registration page completed

--- fundoo-notes/src/Component/SignIn.jsx
+++ fundoo-notes/src/Component/SignIn.jsx
@@ -1,33 +1,29 @@
 import React, {Component} from 'react';
-import '../CSS/Form.css'
+import '../CSS/SignInCss.css';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
-import firebase from '../FirebaseConfig'
-
-const useStyles = makeStyles(theme => ({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: 200,
-    },
-  }));
-
+import firebase from '../FirebaseConfig';
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 export default class Register extends Component{
   constructor(props){
         super(props)
         this.state = {
             emailid: '',
-            password: ''
+            password: '',
+            showPassword: 'false',
+            errors: {},
         };
         this.handleChange = this.handleChange.bind(this);
         this.login = this.login.bind(this);
+        this.submitUserRegistrationForm = this.submitUserRegistrationForm.bind(this);
     }
   
     handleChange(event){
@@ -37,77 +33,173 @@ export default class Register extends Component{
         })
     }
 
-    login(){
+    submitUserRegistrationForm(event) {
+        event.preventDefault();
+        if (this.validateForm()) {
+            this.setState({
+                [event.target.name] : event.target.value,
+                [event.target.formvalid]:  !event.target.formvalid
+            })
+            alert("You have Login Successfully !");
+        }
+    }
 
+    validateForm=()=> {
+
+        let errors = {};
+        var formIsValid = true;
+  
+        if (!this.state.emailid) {
+          formIsValid = false;
+          errors["emailid"] = "*Please enter your email-ID.";
+        }
+  
+        if (typeof this.state.emailid !== "undefined") {
+          //regular expression for email validation
+          var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+          if (!pattern.test(this.state.emailid)) {
+            formIsValid = false;
+            errors["emailid"] = "*Please enter valid email-ID.";
+          }
+        }
+  
+        if (!this.state.password) {
+          formIsValid = false;
+          errors["password"] = "*Please enter your password.";
+        }
+  
+        if (typeof this.state.password !== "undefined") {
+          if (!this.state.password.match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+            formIsValid = false;
+            errors["password"] = "*Please enter secure and strong password.";
+          }
+        }
+  
+        this.setState({
+          errors: errors
+        });
+        
+        return formIsValid;
+    }
+
+    handleClickShowPassword = () => {
+        this.setState({ showPassword: !this.state.showPassword });
+    }
+
+    handleMouseDownPassword = event => {
+        event.preventDefault();
+    };
+
+    login(){
         firebase.auth().signInWithEmailAndPassword(this.state.emailid, this.state.password).then(() => {
             this.props.history.push('/welcome');
         })
         .catch(function(error) {
-            // Handle Errors here.
+            // Handle Errors here
             var errorCode = error.code;
             var errorMessage = error.message;
             console.log('Error code : ' + errorCode); 
             console.log('Error Msg : ' + errorMessage);
             // ...
-          });
+        });
     }
 
-
-  render(){
-      return(
-            <div className="Login" style={{ width: '100%' }}>
-            <form>
-                <div>
-                    <Grid container spacing={1}>    
-                        <Grid item xs={12}>
-                            <TextField 
-                            id="outlined-basic" 
-                            label="Username" 
-                            variant="outlined" 
-                            margin="dense"
-                            value={this.state.emailid} 
-                            onChange={this.handleChange}
-                            name="emailid"
-                            
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={2}>
-                        <Grid item xs={6}>
-                            <TextField 
-                                id="outlined-basic" 
-                                label="Password" 
-                                variant="outlined" 
-                                margin="dense"
-                                value={this.state.password} 
-                                onChange={this.handleChange}
-                                name="password"
-                            />
-                        </Grid>
-                    </Grid>
-                    <Grid container spacing={1}>
-                        <Grid item xs={6}>
-                        <Button variant="contained" 
-                            onClick={()=>{
-                                this.props.history.push('/signup')}}>
-                            Create Account
+    render(){
+        return(
+            <div className="mainContainer">
+                <form onSubmit={this.submitUserRegistrationForm}> 
+                    <div className="titleContainer"> 
+                        <div className="title1">
+                            <span style={{color: '#4285F4'}}>F</span>
+                            <span style={{color: '#DB4437'}}>u</span>
+                            <span style={{color: '#F4B400'}}>n</span>
+                            <span style={{color: '#4285F4'}}>d</span>
+                            <span style={{color: '#0F9D58'}}>o</span>
+                            <span style={{color: '#DB4437'}}>o</span>
+                        </div>
+                        <div className="subtitle1">
+                            <span>Sign in</span>
+                        </div>
+                        <div className="subtitle1">
+                            <span>
+                                Use your Fundoo Account 
+                            </span>
+                        </div>
+                    </div>
+                    <div>
+                        <TextField 
+                        id="outlined-basic" 
+                        label="Email" 
+                        fullWidth
+                        variant="outlined" 
+                        value={this.state.emailid} 
+                        onChange={this.handleChange}
+                        name="emailid"
+                        error={this.state.errors.emailid}
+                        style={{marginTop: 30}} 
+                        helperText={this.state.errors.emailid}
+                    />
+                    </div>
+                    <div style={{marginTop: 10}, {marginBottom: 10}}>
+                        <Button color="primary">
+                            Forgot email?
                         </Button>
-                        </Grid>
-                        <Grid item xs={6}>
+                    </div>
+                    <div>
+                        <FormControl variant="outlined" fullWidth error={this.state.errors.password}>
+                            <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+                            <OutlinedInput
+                                id="outlined-adornment-password"
+                                type={!this.state.showPassword ? 'text' : 'password'}
+                                value={this.state.password}
+                                name="password"
+                                onChange={this.handleChange}
+                                endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={this.handleClickShowPassword}
+                                    onMouseDown={this.handleMouseDownPassword}
+                                    edge="end"
+                                    >
+                                    {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                                    </IconButton>
+                                </InputAdornment>
+                                }
+                                labelWidth={70}
+                            />
+                            <FormHelperText error>{this.state.errors.password}</FormHelperText>
+                        </FormControl>
+                    </div>
+                    <div>
+                        <Button color="primary">
+                            Forgot password?
+                        </Button>
+                    </div>
+                    <div className="btnContain">
+                        <div>
+                            <Button color="primary"
+                                size="large"
+                                type="submit"
+                                onClick={()=>{
+                                    this.props.history.push('/signup')}}>
+                                Create Account
+                            </Button>
+                        </div>
+                        <div>
                             <Button 
                                 variant="contained" 
                                 color="primary" 
+                                size="large"
+                                type="submit"
                                 onClick={this.login}
                             >
                             Next
                             </Button>
-                        </Grid>
-                    </Grid>
-                
-                </div>
-            </form>
-            </div>
-           
-      );
-  }
+                        </div>
+                    </div>
+                </form>
+            </div>    
+        );
+    }
 }
--- fundoo-notes/src/Component/SignUp.jsx
+++ fundoo-notes/src/Component/SignUp.jsx
@@ -1,9 +1,18 @@
 import React, {Component} from 'react';
-import '../CSS/Form.css'
+import '../CSS/SignUpCss.css'
+import Glogo from '../images/account.svg'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import firebase from '../FirebaseConfig';
-import Box from '@material-ui/core/Box'
+import Container from '@material-ui/core/Container'
+import Visibility from '@material-ui/icons/Visibility';
+import VisibilityOff from '@material-ui/icons/VisibilityOff';
+import IconButton from '@material-ui/core/IconButton';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import OutlinedInput from '@material-ui/core/OutlinedInput';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
 export default class Register extends Component{
   constructor(props){
@@ -13,28 +22,114 @@ export default class Register extends Component{
             lastname: '',
             emailid: '',
             password: '',
-            comfirmpassword: '',
-
+            confirmpassword: '',
+            showPassword: 'false',
+            showConfirmPassword: 'false',
+            errors: {},
         };
         this.handleChange = this.handleChange.bind(this);
         this.writeUserData = this.writeUserData.bind(this);
-        
+        this.submitUserRegistrationForm = this.submitUserRegistrationForm.bind(this);
     }
 
     handleChange(event){
-        // const {name, value} = event.target
-        console.log(event.target.name + ' :');
         
         this.setState({
           [event.target.name] : event.target.value
-
-        },()=>{
-            console.log(this.state.firstname + ' :a');
-            
         })
     }
 
+    submitUserRegistrationForm(event) {
+        event.preventDefault();
+        if (this.validateForm()) {
+            this.setState({
+                [event.target.name] : event.target.value,
+                [event.target.formvalid]:  !event.target.formvalid
+            })
+            alert("You have Login Successfully !");
+        }
+    }
+
+    validateForm=()=> {
+
+        let errors = {};
+        var formIsValid = true;
+
+        if (!this.state.firstname) {
+            formIsValid = false;
+            errors["firstname"] = "*Please enter your First Name.";
+        }
+
+        if (!this.state.lastname) {
+            formIsValid = false;
+            errors["lastname"] = "*Please enter your Last Name.";
+        }
+  
+        if (!this.state.emailid) {
+          formIsValid = false;
+          errors["emailid"] = "*Please enter your email-ID.";
+        }
+  
+        if (typeof this.state.emailid !== "undefined") {
+          //regular expression for email validation
+          var pattern = new RegExp(/^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/i);
+          if (!pattern.test(this.state.emailid)) {
+            formIsValid = false;
+            errors["emailid"] = "*Please enter valid email-ID.";
+          }
+        }
+  
+        if (!this.state.password) {
+          formIsValid = false;
+          errors["password"] = "*Please enter your password.";
+        }
+  
+        if (typeof this.state.password !== "undefined") {
+          if (!this.state.password.match(/^.*(?=.{8,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%&]).*$/)) {
+            formIsValid = false;
+            errors["password"] = "*Please enter secure and strong password.";
+          }
+        }
+
+        if (!this.state.confirmpassword) {
+            formIsValid = false;
+            errors["confirmpassword"] = "*Please confirm your password.";
+        }
+    
+        if (typeof this.state.confirmpassword !== "undefined") {
+            if (this.state.confirmpassword !== this.state.password) {
+                formIsValid = false;
+                errors["confirmpassword"] = "*Password does not match.";
+            }
+        }
+  
+        this.setState({
+          errors: errors
+        });
+        
+        return formIsValid;
+    }
+
+
+    handleClickShowPassword = () => {
+        this.setState({ 
+            showPassword: !this.state.showPassword,
+            showConfirmPassword: !this.state.showConfirmPassword
+        });
+    };
+
+    handleClickShowConfirmPassword = () => {
+        this.setState({ 
+            showConfirmPassword: !this.state.showConfirmPassword 
+        });
+    };
+
+    handleMouseDownPassword = event => {
+        event.preventDefault();
+    };
+
     writeUserData(){
+
         firebase.auth().createUserWithEmailAndPassword(this.state.emailid, this.state.password).then(()=>{
             firebase.database().ref('users/').push({
                 firstname: this.state.firstname,
@@ -54,25 +149,42 @@ export default class Register extends Component{
         });
     }
 
-  render(){
-      return(
-            <div>
-                <form>
-                    <Box border={1}> 
+    render(){
+        return(
+            <Container maxWidth="lg">
+            <div className="main-container">
+ 
+                <div className="col-container"> 
+                    <form onSubmit={this.submitUserRegistrationForm}>
+                        <div className="title">
+                            <span style={{color: '#4285F4'}}>F</span>
+                            <span style={{color: '#DB4437'}}>u</span>
+                            <span style={{color: '#F4B400'}}>n</span>
+                            <span style={{color: '#4285F4'}}>d</span>
+                            <span style={{color: '#0F9D58'}}>o</span>
+                            <span style={{color: '#DB4437'}}>o</span>
+                        </div>
+                        <div className="subtitle">
+                            <span>Create your Fundoo Account</span>
+                        </div>
+                        <div className="row-container">
 
-                        <Box>
-                            <Box>
+                            <div>
                                 <TextField 
-                                    id="outlined-basic" 
+                                    id="outlined-basic"
                                     label="First name" 
                                     variant="outlined" 
                                     margin="dense"
                                     value={this.state.firstname} 
                                     onChange={this.handleChange}
                                     name="firstname"
+                                    error={this.state.errors.firstname}
+                                    helperText={this.state.errors.firstname}
+                                    className="item1"
                                 />
-                            </Box>
-                            <Box>
+                            </div>
+
+                            <div>
                                 <TextField 
                                     id="outlined-basic" 
                                     label="Last name" 
@@ -81,70 +193,117 @@ export default class Register extends Component{
                                     onChange={this.handleChange}
                                     name="lastname"
                                     margin="dense"
+                                    error={this.state.errors.lastname}
+                                    helperText={this.state.errors.lastname}
+                                    className="item2"
                                 />
-                            </Box>
-                        </Box>
+                            </div>
 
-                        <Box>
+                        </div>
+
+                        <div>
                             <TextField 
-                                id="outlined-basic" 
-                                fullWidth
+                                id="outlined-basic"
                                 label="Email" 
                                 variant="outlined" 
                                 value={this.state.username} 
                                 onChange={this.handleChange}
                                 name="emailid"
                                 margin="dense"
+                                error={this.state.errors.emailid}
+                                helperText={this.state.errors.emailid ? 
+                                    this.state.errors.emailid
+                                    : "You can use letters, numbers & periods"}
+                                className="item3"
                             />
-                        </Box>
+                        </div>
 
-                        <Box>
-                            <Box>
+                        <div className="row-container">
+
+                            <div>
                                 <TextField 
-                                    id="outlined-basic" 
+                                    id="outlined-basic"
                                     label="Password" 
+                                    type={!this.state.showPassword ? 'text' : 'password'}
+                                    // style={{ margin: 10 },{width:170}}
+                                    size="small"
                                     variant="outlined" 
-                                    margin="dense"
                                     value={this.state.password} 
                                     onChange={this.handleChange}
                                     name="password"
-                                />
-                            </Box>
-                            <Box>
-                                <TextField 
-                                    id="outlined-basic" 
-                                    label="Confirm"
-                                    variant="outlined" 
                                     margin="dense"
-                                    value={this.state.comfirmpassword} 
-                                    onChange={this.handleChange}
-                                    name="comfirmpassword"
+                                    error={this.state.errors.password}
+                                    helperText={this.state.errors.password}
+                                    className="item4"
                                 />
-                            </Box>
-                        </Box>
+                            </div>
 
-                        <Box>
-                            <Box>
-                                <Button variant="contained" 
+                            <div>
+                                <TextField 
+                                    id="outlined-basic"
+                                    label="Confirm" 
+                                    type={!this.state.showPassword ? 'text' : 'password'}
+                                    //style={{ marginLeft: 10 },{width:170}}
+                                    variant="outlined" 
+                                    value={this.state.confirmpassword} 
+                                    onChange={this.handleChange}
+                                    name="confirmpassword"
+                                    margin="dense"
+                                    error={this.state.errors.confirmpassword}
+                                    helperText={this.state.errors.confirmpassword}
+                                    className="item5"
+                                />
+                                <IconButton
+                                aria-label="toggle password visibility"
+                                onClick={this.handleClickShowPassword}
+                                onMouseDown={this.handleMouseDownPassword}>
+                                {this.state.showPassword ? <Visibility /> : <VisibilityOff />}
+                            </IconButton>
+                            </div>
+                        </div>
+                        <div className="passSpan">
+                            <span>
+                                Use 8 or more characters with a mix of letters, numbers & symbols
+                            </span>
+                        </div>
+
+                        <div className="row-container">
+                            
+                            <div className="item">
+                                <Button
+                                    color="primary"
+                                    size="large"
                                     onClick={()=>{
-                                        this.props.history.push('/')}}>
+                                        this.props.history.push('/')}}
+                                        >
                                 Sign in instead
                                 </Button>
-                            </Box>
-                            <Box>
+                            </div>
+
+                            <div className="item">
                                 <Button 
-                                        variant="contained" 
-                                        color="primary" 
-                                        onClick={this.writeUserData}>
+                                    variant="contained" 
+                                    size="large"
+                                    color="primary" 
+                                    type="submit"
+                                    onClick={this.writeUserData}>
                                 Next
                                 </Button>
-                            </Box>
-                        </Box>
+                            </div>
 
-                    </Box>
+                        </div>
                 </form>
             </div>
-           
+            <div className="glogo col-container">
+                <img src={Glogo} alt="fundoo logo" />
+                <div>
+                    <p style={{textAlign: 'center'}}>
+                    One account. All of Google working for you.
+                    </p>
+                </div>
+            </div> 
+        </div>
+        </Container>
       );
   }
 }
